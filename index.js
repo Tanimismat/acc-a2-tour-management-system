97c59c2ccca8895d07167f6b6a5bf251b4d9fc8b
Tanim Ismat <tanim.ismat@gmail.com>
2022-09-19 11:20:59 +0600

    server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.send("Route is working");
+});
+
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+	console.log("Database connection is successful");
+});
+
+app.listen(port, () => {
+	console.log(`App is running on the port ${port}`);
+});
